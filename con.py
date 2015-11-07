0b3839cde077233ae881a4fa4c2c336ae19768dc
Daniel Bastos <danielfloresbastos@gmail.com>
2015-11-07 00:30:22 -0200

    Update mongo connection.

--- con.py
+++ con.py
@@ -1,18 +1,12 @@
 import os
 from pymongo import MongoClient
 
-dbhost = os.environ.get('OPENSHIFT_MONGODB_DB_HOST', 'localhost')
-dbport = os.environ.get('OPENSHIFT_MONGODB_DB_PORT', 27017)
-dbuser = os.environ.get('OPENSHIFT_MONGODB_DB_USERNAME', None)
-dbpass = os.environ.get('OPENSHIFT_MONGODB_DB_PASSWORD', None)
+dburl = os.environ.get('OPENSHIFT_MONGODB_DB_URL', None)
+if not dburl:
+    dburl = 'mongodb://localhost:27017/'
+dburl += 'bitcoinsnotification'
 
-if dbuser:
-    uri = 'mongodb://{0}:{1}@{2}:{3}/bitcoinsnotification'.format(
-        dbuser, dbpass, dbhost, dbport)
-else:
-    uri = 'mongodb://{0}:{1}/bitcoinsnotification'.format(dbhost, dbport)
-
-client = MongoClient(uri)
+client = MongoClient(dburl, tz_aware=True)
 db = client.get_default_database()
 
 foxbit = db['foxbit']
